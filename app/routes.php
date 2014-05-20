769199340972f6f3ac6729cfa148dd9f58fd49ef
Emily Stamey <elstamey@ncsu.edu>
2014-05-20 17:17:36 -0400

    added admin route

--- app/routes.php
+++ app/routes.php
@@ -11,3 +11,13 @@ Route::get('login', ['as' => 'login', 'uses' => 'SessionsController@create']);
 Route::post('login', 'SessionsController@store');
 
 Route::get('logout', ['as' => 'logout', 'uses' => 'SessionsController@destroy']);
+
+// Add the admin route
+Route::group(['before' => 'auth'], function()
+{
+    //Defer entire route to resource controller
+    Route::resource('admin/posts', 'PostsController');
+
+    //Defer entire route to controller
+    Route::controller('admin', 'AdminController');
+});
