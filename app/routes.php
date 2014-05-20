d9d6c5b3c89dc107d777eba38d584214d29a6e2a
Emily Stamey <elstamey@ncsu.edu>
2014-05-20 17:11:34 -0400

    added login/logout to routes

--- app/routes.php
+++ app/routes.php
@@ -5,3 +5,9 @@ Route::get('/', ['as' => 'home', function()
 {
     return View::make('home');
 }]);
+
+//Explicit routes mapped to controller methods
+Route::get('login', ['as' => 'login', 'uses' => 'SessionsController@create']);
+Route::post('login', 'SessionsController@store');
+
+Route::get('logout', ['as' => 'logout', 'uses' => 'SessionsController@destroy']);
